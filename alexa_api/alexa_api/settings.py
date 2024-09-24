90a2f34ba37e0a57813351ebe6c588652430055b
shibil-thora <shibilallakkat123@gmail.com>
2024-09-24 11:50:00 +0530

    added requirements

--- alexa_api/alexa_api/settings.py
+++ alexa_api/alexa_api/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path 
-import os
+import os 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,7 @@ DATABASES = {
 } 
 
 if os.environ.get('LOCAL') != 'TRUE': 
-    DATABASES['default'] = None
+    DATABASES['default'] = dj_database_url.parse(os.environ.get('DB_URL'))
 
 
 # Password validation
