c614f60f976a4f1d66aaa5940fae80dccf00422d
shibil-thora <shibilallakkat123@gmail.com>
2024-09-24 12:11:06 +0530

    updated view as alexa wanted

--- alexa_api/api/views.py
+++ alexa_api/api/views.py
@@ -2,24 +2,29 @@ from django.shortcuts import render, redirect
 from rest_framework.views import APIView 
 from rest_framework.response import Response   
 from rest_framework.permissions import IsAuthenticated 
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login 
+import string 
+import random
 
 
 class HomeAPI(APIView): 
     def get(self, request): 
         return Response('hi') 
     
+def generate_authorization_code():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=30))
 
 def user_login(request):  
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)  
-        print(user, username, password, 'here')
-        if user is not None: 
-            print(user, 'coming here')
+        if user is not None:  
+            state = request.POST.get('state') 
+            redirect_uri = request.POST.get('redirect_uri') 
+            authorization_code = generate_authorization_code()
             auth_login(request, user)
-            return redirect('home')
+            return redirect(f'{redirect_uri}?code={authorization_code}&state={state}')
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials'})
     return render(request, 'login.html')
